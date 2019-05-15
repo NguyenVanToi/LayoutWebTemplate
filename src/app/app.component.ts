8ea4663962134a45b24450bf341aa184cca807c4
Nguyen Van Toi <toinv2610@gmail.com>
2019-05-15 23:41:35 +0700

    add component dashboard, header and menu left

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
 import { Component } from '@angular/core';
+import {trigger, state, style, transition, animate} from '@angular/animations'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
+  animations: [
+    trigger('itemDropdown', [
+        state('up', style({
+          "height": '0px',
+          "overflow-y": 'hidden',
+        })),
+        state('down', style({
+          "min-height": '120px',
+        })),
+        transition('up => down', animate('300ms ease-in-out')), 
+        transition('down => up', animate('300ms ease-in-out')), 
+    ])
+  ]
 })
 export class AppComponent {
   title = 'angular-layout';
+
+  itemMenu = [
+    {name: 'Web Analytics', link: 'web-analytics'},
+    {name: 'Sales Monitoring', link: 'sales-monitoring'},
+    {name: 'Ad Campaign', link: 'wad-campaign'},
+    {name: 'Helpdesk Management', link: 'helpdesk-management'},
+    {name: 'Finance Monitoring', link: 'finance-monitoring'},
+    {name: 'Campaign Monitoring', link: 'campaign-monitoring'},
+    {name: 'Excutive SaaS', link: 'excutive-saas'},
+    {name: 'Product Management', link: 'product-management'},
+    {name: 'Smarthome', link: 'smarthome'}
+  ];
+
+  menuState:string = 'up';
+  menuState3:string = 'up';
+  menuState4:string = 'up';
+  menuState5:string = 'up';
+  menuState6:string = 'up';
+  ngOnInit() {
+  }
+
+  activeDropdown(numMenu : string) 
+  {
+    switch (numMenu)
+    {
+      case '1':
+        this.menuState = this.menuState === 'down' ? 'up' : 'down';
+        break;
+      case '3':
+        this.menuState3 = this.menuState3 === 'down' ? 'up': 'down';
+        break;
+      case '4':
+        this.menuState4 = this.menuState4 === 'down' ? 'up': 'down';
+        break;
+      case '5':
+        this.menuState5 = this.menuState5 === 'down' ? 'up': 'down';
+        break;
+      case '6':
+        this.menuState6 = this.menuState6 === 'down' ? 'up': 'down';
+        break;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,32 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { 
+  MatMenuModule,
+  MatToolbarModule, 
+  MatIconModule, 
+  MatSidenavModule,
+  MatTreeModule,
+  MatTableModule
+} from '@angular/material';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule
+    NoopAnimationsModule, 
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatTreeModule,
+    MatTableModule
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
